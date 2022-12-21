f9517d58d6cd37441bd66349f613170667eddc63
belovetech <belovetech@gmail.com>
2022-12-21 20:47:56 +0100

    [Update]: add comment

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -36,14 +36,16 @@ def pascal_triangle(n):
 #         n (int): Size of the pascal triangle
 #     """
 
-#     for i in range(n+1):
+#     for i in range(1, n+1):
 #         for j in range(n-i):
 #             print(' ', end='')
+#         # first element is always 1
 #         c = 1
 #         for j in range(1, i+1):
+#             # first value in a line is always 1
 #             print(c, ' ', sep='', end='')
+#             # using Binomial Coefficient
 #             c = c * (i - j) // j
 #         print()
-
 
 # pascal_triangle2(5)
