9649aa95764260e5ba47c4d1a9f6a7b5c295625d
belovetech <belovetech@gmail.com>
2023-01-06 18:37:17 +0100

    [Feat]: solving the same problem with set

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -21,3 +21,24 @@ def canUnlockAll(boxes):
     if len(unlocked) == len(boxes):
         return True
     return False
+
+
+def canUnlockAll2(boxes):
+    """Check if all boxes can be opened
+
+    Args:
+        boxes (arr[int]): Boxes contains the key to open other boxes
+
+    Return:
+        (bool): True otherwise false
+    """
+    unlocked = set()
+    for boxID, box in enumerate(boxes):
+        if len(box) == 0 or boxID == 0:
+            unlocked.add(boxID)
+        for key in box:
+            if key < len(boxes) and key != boxID:
+                unlocked.add(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
--- 0x01-lockboxes/0-main.py
+++ 0x01-lockboxes/0-main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
+canUnlockAll2 = __import__('0-lockboxes').canUnlockAll2
 
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes))
+print(canUnlockAll2(boxes))
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 print(canUnlockAll(boxes))
+print(canUnlockAll2(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+print(canUnlockAll2(boxes))
