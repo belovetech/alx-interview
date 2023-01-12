87ac7a3ff1c9290227b404358e0c42328a4bf9cb
belovetech <belovetech@gmail.com>
2023-01-12 10:46:14 +0100

    [Feat]: calculate minimum operation

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 8
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 10
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 11
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 122
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env pyhton3
+"""calculates the fewest number of operations needed to result
+    in exactly n H characters in the file
+"""
+
+
+def minOperations(n: int) -> int:
+    """Minimum operation"""
+    if n == 0 or n == 1:
+        return 0
+
+    prev = 1
+    operations = cur = value = 2
+
+    while n > value:
+        reminder = n - value
+
+        if reminder % cur == 0:
+            operations += 2
+            prev = cur
+            value += cur
+            cur = value
+        elif reminder % prev == 0:
+            operations += 1
+            value += prev
+            cur = value
+        else:
+            return 0
+
+    return operations
