25b4715f4424b59d5216a95519def97bf73d8c92
belovetech <belovetech@gmail.com>
2023-01-19 21:05:29 +0100

    [Update]: raise keyboard interrupt exception

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,12 +5,12 @@ Write a script that reads stdin line by line and computes metrics:
 import sys
 
 
+total_file_size = 0
+status = [200, 301, 400, 401, 403, 404, 405, 500]
+obj = dict.fromkeys(status, 0)
+
 if __name__ == "__main__":
     count = 0
-    total_file_size = 0
-    status = [200, 301, 400, 401, 403, 404, 405, 500]
-    obj = dict.fromkeys(status, 0)
-
     try:
         for line in sys.stdin:
             try:
