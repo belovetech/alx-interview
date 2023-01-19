69e5da635adde2b85ee00b095ed0330d800dfafc
belovetech <belovetech@gmail.com>
2023-01-19 21:29:18 +0100

    [Update]: raise keyboard interrupt exception

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,28 +9,34 @@ total_file_size = 0
 status = [200, 301, 400, 401, 403, 404, 405, 500]
 obj = dict.fromkeys(status, 0)
 
+def printLogStat():
+    """Print log statistics"""
+    print("File size: {}".format(total_file_size))
+    for key, value in obj.items():
+        if value > 0:
+            print("{}: {}".format(key, value))
+
 if __name__ == "__main__":
     count = 0
     try:
         for line in sys.stdin:
             try:
                 line = line.split()
-                total_file_size += int(line[8])
+                total_file_size += int(line[-1])
 
-                code = int(line[7])
+                code = int(line[-2])
                 if code in status:
                     obj[code] = obj.get(code, 0) + 1
 
-                count += 1
             except Exception:
                 pass
+            count += 1
 
             if count % 10 == 0:
-                print("File size: {}".format(total_file_size))
-
-                for key, value in obj.items():
-                    if value > 0:
-                        print("{}: {}".format(key, value))
+                printLogStat()
+                
     except KeyboardInterrupt:
-        print("File size: {}".format(total_file_size))
+        printLogStat()
         raise
+    else:
+        printLogStat()
