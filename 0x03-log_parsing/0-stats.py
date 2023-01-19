83d283e05211720558f841c03ccaef94c15335a0
belovetech <belovetech@gmail.com>
2023-01-19 20:56:28 +0100

    [Update]: raise keyboard interrupt exception

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,16 +3,6 @@
 Write a script that reads stdin line by line and computes metrics:
 """
 import sys
-import signal
-
-
-def handler(signum, frame):
-    """Keyboard interruption handler"""
-    print(f"File size: {total_file_size}")
-    exit(0)
-
-
-signal.signal(signal.SIGINT, handler)
 
 
 if __name__ == "__main__":
@@ -21,22 +11,26 @@ if __name__ == "__main__":
     status = [200, 301, 400, 401, 403, 404, 405, 500]
     obj = dict.fromkeys(status, 0)
 
-    for line in sys.stdin:
-        if 'Exit' == line.rstrip():
-            break
+    try:
+        for line in sys.stdin:
+            if 'Exit' == line.rstrip():
+                break
 
-        count += 1
+            count += 1
 
-        line = line.split()
-        total_file_size += int(line[8])
+            line = line.split()
+            total_file_size += int(line[8])
 
-        code = int(line[7])
-        if code in status:
-            obj[code] = obj.get(code, 0) + 1
+            code = int(line[7])
+            if code in status:
+                obj[code] = obj.get(code, 0) + 1
 
-        if count % 10 == 0:
-            print(f"File size: {total_file_size}")
+            if count % 10 == 0:
+                print(f"File size: {total_file_size}")
 
-            for key, value in obj.items():
-                if value > 0:
-                    print(f"{key}: {value}")
+                for key, value in obj.items():
+                    if value > 0:
+                        print(f"{key}: {value}")
+    except KeyboardInterrupt:
+        print(f"File size: {total_file_size}")
+        raise
