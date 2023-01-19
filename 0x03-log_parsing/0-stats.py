9ab624742dfbd6db0a61fdd5f7eafbecf0600580
belovetech <belovetech@gmail.com>
2023-01-19 16:25:17 +0100

    [Feat]: log parsing after every 10 lines

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,27 +15,28 @@ def handler(signum, frame):
 signal.signal(signal.SIGINT, handler)
 
 
-count = 0
-total_file_size = 0
-status = [200, 301, 400, 401, 403, 404, 405, 500]
-obj = dict.fromkeys(status, 0)
+if __name__ == "__main__":
+    count = 0
+    total_file_size = 0
+    status = [200, 301, 400, 401, 403, 404, 405, 500]
+    obj = dict.fromkeys(status, 0)
 
-for line in sys.stdin:
-    if 'Exit' == line.rstrip():
-        break
+    for line in sys.stdin:
+        if 'Exit' == line.rstrip():
+            break
 
-    count += 1
+        count += 1
 
-    line = line.split()
-    total_file_size += int(line[8])
+        line = line.split()
+        total_file_size += int(line[8])
 
-    code = int(line[7])
-    if code in status:
-        obj[code] = obj.get(code, 0) + 1
+        code = int(line[7])
+        if code in status:
+            obj[code] = obj.get(code, 0) + 1
 
-    if count % 10 == 0:
-        print(f"File size: {total_file_size}")
+        if count % 10 == 0:
+            print(f"File size: {total_file_size}")
 
-        for key, value in obj.items():
-            if value > 0:
-                print(f"{key}: {value}")
+            for key, value in obj.items():
+                if value > 0:
+                    print(f"{key}: {value}")
