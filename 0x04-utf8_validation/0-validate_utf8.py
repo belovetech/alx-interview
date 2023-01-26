cdaa8fbf3621517f7ae20033d55289fc21eef0f1
belovetech <belovetech@gmail.com>
2023-01-26 23:26:14 +0100

    [Feat]: determine valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
 """UTF-8 Validation
-Determines if a given data set represents a valid UTF-8 encoding.
 """
-
-
-def getType(num):
-    """Check the type
-    """
-    masks = [128, 64, 32, 16, 8]
-    for i in range(len(masks)):
-        if (masks[i] & num) == 0:
-            return i
-    return -1
 
 
 def validUTF8(data):
     """Determines if a given data set represents
      a valid UTF-8 encoding or not
     """
-    for i in range(len(data)):
-        curr = data[i]
-        _type = getType(curr)
-
-        if _type == 0:
-            continue
-        elif _type > 1 and i + _type <= len(data):
-            while _type > 1:
-                _type -= 1
-                i += 1
-                if getType(data[i]) != 1:
-                    return False
+    count = 0
+    for d in data:
+        if count == 0:
+            if d & 128 == 0:
+                count = 0
+            elif d & 224 == 192:
+                count = 1
+            elif d & 240 == 224:
+                count = 2
+            elif d & 248 == 240:
+                count = 3
+            else:
+                return False
         else:
-            return True
-    return True
+            if d & 192 != 128:
+                return False
+            count -= 1
+    if count == 0:
+        return True
+    return False
